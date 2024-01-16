0508fde2cd64a3fba1cea24d58fabfe26c57ff83
Matthias Hryniszak <padcom@gmail.com>
2024-01-16 13:06:59 +0100

    Initial import

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Hello, world! {{ message }} {{ text }}</h1>
+  <input v-model="text" type="text" @input="console.log('input type=text:', $event)">
+  <textarea v-model="text" @input="console.log('textarea:', $event)" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+defineProps({
+  message: { type: String, default: 'from HelloWorld.vue' },
+})
+
+const text = ref('change me')
+</script>
+
+<style lang="postcss" scoped>
+h1 {
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+}
+</style>
